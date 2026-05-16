9be330cccbb611b190f06fa8dccc55a05c6aff88
Ayush Srivastava <59785377+miracneroid@users.noreply.github.com>
2026-05-16 05:47:50 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/work/adsyncai/",
   server: {
     host: "::",
     port: 8080,
